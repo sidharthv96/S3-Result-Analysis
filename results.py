e1e88a6a3df756f03accd736a937e8ba8cecb215
sidharth_vinod <sidharthv96@gmail.com>
2016-12-02 10:40:42 +0530

    Final Result

--- results.py
+++ results.py
@@ -1,30 +1,35 @@
 import requests
 import thread
-import md5
 from multiprocessing import Pool
 import random
 import os
 
-# Define a function for the thread
 def download_result(regno):
-    id = str(md5.new(str(random.random())).hexdigest())[:26]
     reg = "ETAOECS"+str(regno).zfill(3)
-    print reg,id
-    headers = {"Cookie": "PHPSESSID="+id}
-    data = {"regno": reg, 'sum': '111', 'id': '5659', 'sessionok': 'yes'}
+    try:
+        id = (str(random.random())[2:]*27)[:26]
+        print reg,id
+        headers = {"Cookie": "PHPSESSID="+id}
+        data = {"regno": reg, 'sum': '111', 'id': '5659', 'sessionok': 'yes'}
 
-    r = requests.post('http://61.0.254.181/CuPbhavan/res_newregentry.php', data=data, headers=headers)
-    r = requests.post('http://61.0.254.181/CuPbhavan/rs_newcheck.php', headers=headers)
-    r = requests.post('http://61.0.254.181/CuPbhavan/cubtech7/barch_res.php', headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/res_newregentry.php', data=data, headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/rs_newcheck.php', headers=headers)
+        r = requests.post('http://61.0.254.181/CuPbhavan/cubtech7/barch_res.php', headers=headers)
 
-    with open("PDF/"+str(regno)+".pdf", 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    print "Saved : "+reg
+        with open("PDF/"+str(regno)+".pdf", 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        print "Saved : "+reg
+    except Exception as e:
+        print e
+        print "Failed : "+reg        
 
 try:
-    os.mkdir("PDF")
+    if not os.path.exists("PDF"):
+        os.makedirs("PDF")
+    rolls = list(set(range(1, 70)) - set(map(lambda x: int(x[:-4]), (os.listdir("PDF")))))
+    print rolls
     p = Pool(10)
-    p.map(download_result,range(1,70))
+    p.map(download_result,rolls)
 except Exception as e:
     print e.message
